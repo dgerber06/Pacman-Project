5d8929f6c8c6bf6582b51905b919b68822a42131
ShadowPikmin <jdcarter432@gmail.com>
2026-02-23 13:21:16 -0600

    Commits new implementation of Q7 before pulling Q5

--- searchAgents.py
+++ searchAgents.py
@@ -472,14 +472,24 @@ def foodHeuristic(state: Tuple[Tuple, List[List]], problem: FoodSearchProblem):
     if problem.isGoalState(state):
         return 0 
 
-    x = 0  
     cost = 0
-    for food in foodGrid.asList():
-        cost = cost + util.manhattanDistance(position, food)
-        x = x + 1
 
-    return (cost / x)
+    food_list = foodGrid.asList()
+    cur_pos = position
+    i = len(food_list)
 
+    while not(len(food_list) == 0):
+          min_food, min_dist = (None, -1)
+          for food in food_list:
+              dist = util.manhattanDistance(cur_pos, food)
+              if dist < min_dist or min_dist == -1 :
+                  min_food, min_dist = (food, dist)
+		  
+          cost = cost + min_dist
+          cur_pos = min_food
+          food_list.remove(min_food)
+    
+    return cost / 2 
 
 class ClosestDotSearchAgent(SearchAgent):
     "Search for all food using a sequence of searches"
